0994340c852427270151bc23288f2c0a34a8eb36
frank <frank.chen@skylinetw.com>
2020-03-24 18:14:09 +0800

    [refactor] 移除目前沒有用到的custom hook內容

--- src/hook/index.js
+++ src/hook/index.js
@@ -1,21 +1,3 @@
 import { useState, useEffect, useCallback, useRef } from "react"
 
-window.TIMER = null
-
-export const useClock = ({}) => {
-  const [isPlay, setIsPlay] = useState(false)
-  const [isBell, setIsBell] = useState(true)
-  const [time, setTime] = useState(0)
-
-  const onBtnPlay = () => {
-    time > 0 && setIsPlay(prevIsPlay => !prevIsPlay)
-  }
-
-  const onBtnBell = () => {
-    setIsBell(prevIsBell => !prevIsBell)
-  }
-
-  useEffect(() => {}, [])
-
-  return [isBell, isPlay, time, progress, onBtnBell, onBtnPlay]
-}
+export const useClock = () => {}
