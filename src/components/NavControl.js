e0c51dc30e1a4135b3645a6e3374fbe70405b839
frank <frank.chen@skylinetw.com>
2020-04-10 16:35:01 +0800

    [feat] nav control 改為 styled component & 抽離檔案

--- src/components/NavControl.js
+++ src/components/NavControl.js
@@ -1,54 +1,18 @@
-import React, { memo, useMemo } from "react"
-import classNames from "classnames"
+import React, { memo } from "react"
 import cons from "../constants"
+import { StyledNavControl, BtnList, BtnChart, BtnConfig } from "../style/NavControl"
 
 const NavControl = ({ isOpen = false, currentNavContentId, onShowMenu }) => {
   const handleNavBtn = navContentId => {
     onShowMenu(navContentId)
   }
 
-  const renderNavControlClass = useMemo(
-    () =>
-      classNames({
-        "nav-control": true,
-        "is-open": isOpen
-      }),
-    [isOpen]
-  )
-
-  const renderBtnListClass = useMemo(
-    () =>
-      classNames({
-        "btn-list": true,
-        "is-current": currentNavContentId === cons.LIST_SECTION
-      }),
-    [currentNavContentId]
-  )
-
-  const renderBtnChartClass = useMemo(
-    () =>
-      classNames({
-        "btn-chart": true,
-        "is-current": currentNavContentId === cons.CHART_SECTION
-      }),
-    [currentNavContentId]
-  )
-
-  const renderBtnConfigClass = useMemo(
-    () =>
-      classNames({
-        "btn-config": true,
-        "is-current": currentNavContentId === cons.CONFIG_SECTION
-      }),
-    [currentNavContentId]
-  )
-
   return (
-    <nav className={renderNavControlClass}>
-      <div className={renderBtnListClass} onClick={() => handleNavBtn(cons.LIST_SECTION)}></div>
-      <div className={renderBtnChartClass} onClick={() => handleNavBtn(cons.CHART_SECTION)}></div>
-      <div className={renderBtnConfigClass} onClick={() => handleNavBtn(cons.CONFIG_SECTION)}></div>
-    </nav>
+    <StyledNavControl isOpen={isOpen}>
+      <BtnList isCurrent={currentNavContentId === cons.LIST_SECTION} onClick={() => handleNavBtn(cons.LIST_SECTION)} />
+      <BtnChart isCurrent={currentNavContentId === cons.CHART_SECTION} onClick={() => handleNavBtn(cons.CHART_SECTION)} />
+      <BtnConfig isCurrent={currentNavContentId === cons.CONFIG_SECTION} onClick={() => handleNavBtn(cons.CONFIG_SECTION)} />
+    </StyledNavControl>
   )
 }
 
